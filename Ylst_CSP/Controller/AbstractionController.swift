2e42e5a7c2281208ebfac167ca2dfb510e3fc8eb
ZachYlst <ZachYlst@users.noreply.github.com>
2017-11-27 08:31:36 -0700

    "Swipe left" method completed.

--- Ylst_CSP/Controller/AbstractionController.swift
+++ Ylst_CSP/Controller/AbstractionController.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 
-class AbstractionController: UIViewController
+class AbstractionController: UIPageViewController, UIPageViewControllerDataSource
 {
+    //MARK: Array of subviews
+    private (set) lazy var orderedAbstractionViews: [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+        ]
+    }()
+    
+    //Helper method to retrieve the correct Viewontroller
+    private func newAbstractionViewController(abstractionLevel: String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
+    
+    //MARK:- Required Protocol methods for UIPageViewControllerDataSource
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        return nil
+    }
+    
+    
     override public func viewDidLoad()
     {
         super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
     
     override public func didReceiveMemoryWarning()
