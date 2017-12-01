e4018bc88b0f658bf0a6a3cb3f6a1bc3751a566d
ZachYlst <ZachYlst@users.noreply.github.com>
2017-12-01 09:33:02 -0700

    Worked on InternetDetailViewController.

--- Ylst_CSP/Controller/InternetDetailViewController.swift
+++ Ylst_CSP/Controller/InternetDetailViewController.swift
@@ -15,11 +15,65 @@ class InternetDetailViewController: UIViewController
     @IBOutlet weak var textView: UILabel!
     @IBOutlet weak var webViewer: WKWebView!
     
+    var detailAddress: String?
+    {
+        //MARK: Update the detail when a value is changed
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    
+    var detailText: String?
+    {
+        //MARK: Update the detail view when a value is changed
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    
     override public func viewDidLoad()
     {
         super.viewDidLoad()
-        setup()
+        configureDetailView()
         // Do any additional setup after loading the view
+    }
+    
+    private func configureDetailView() -> Void
+    {
+        if detailAddress != nil
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        }
+        else
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: "https://www.cnn.com")
+                currentWebView.load(URLRequest(url: currentURL!))
+            }
+        }
+        
+        if detailText != nil
+        {
+            if let currentText = textView
+            {
+                currentText.text = detailText
+            }
+        }
+        else
+        {
+            if let currentText = textView
+            {
+                currentText.text = "Zachary's CSP app internet screen"
+            }
+        }
     }
     
     override public func didReceiveMemoryWarning()
@@ -28,4 +82,7 @@ class InternetDetailViewController: UIViewController
     }
     
     private func setup() -> Void
+    {
+        
+    }
 }
--- Ylst_CSP/Controller/InternetMasterViewController.swift
+++ Ylst_CSP/Controller/InternetMasterViewController.swift
@@ -22,11 +22,27 @@ class InternetMasterViewController: UITableViewController
         ]
     }()
     
+    private lazy var addresses: [String] = []
+
     private var detailViewController: InternetDetailViewController?
     
     private func setup() -> Void
     {
+        //TODO: Replace with your correct links
+        addresses = [
+            "https://www.google.com",
+            "https://www.canyons.instructure.com/courses/1137392",
+            "https://www.ctec.canyonsdistrict.org",
+            "https://www.canyonsdistrict.org",
+            "https://www.twitter.com",
+            "https://www.developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language"
+        ]
         
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as? InternetDetailViewController
+        }
     }
     
     override public func viewDidLoad()
@@ -57,6 +73,37 @@ class InternetMasterViewController: UITableViewController
         cell.textLabel!.text = currentText
         
         return cell
+    }
+    
+    //MARK: Handle the internal transfer
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier! == "showDetail"
+        {
+            if let indexPath = self.tableView.indexPathForSelectedRow
+            {
+                let urlString = addresses[indexPath.row]
+                let pageText: String
+                
+                if indexPath.row == 0
+                {
+                    //TODO: Replace with your definitions - eat time to use the """ operator
+                    pageText = "All the definitions you wrote..."
+                }
+                else
+                {
+                    pageText = internetTopics[indexPath.row]
+                }
+                
+                let controller = segue.destination as!
+                    InternetDetailViewController
+                
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
     }
     
     override public func didReceiveMemoryWarning()
