37c6218437520cc6f18d837de5847346f7baba46
ZachYlst <ZachYlst@users.noreply.github.com>
2017-12-07 08:11:52 -0700

    Began work on CreativityController.

--- Ylst_CSP/Controller/CreativityController.swift
+++ Ylst_CSP/Controller/CreativityController.swift
@@ -5,11 +5,63 @@
 //  Created by Ylst, Zachary on 10/26/17.
 //  Copyright © 2017 CTEC. All rights reserved.
 //
-
+//MARK: Shows a bolded pulldown menu comment
 import UIKit
-
-class CreativityController: UIViewController
+//MARK: - Class type should inherit from UICollectionViewController
+class CreativityController: UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow: CGFloat = 3
+    
+    private lazy var artSelection: [UIImage?] =
+    {
+        return [
+            UIImage(named: "tree"),
+            UIImage(named: "myArt"),
+            UIImage(named: "tree"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt"),
+            UIImage(named: "myArt")
+        ]
+    }()
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            
+            collectionView?.performBatchUpdates(
+                {
+                    self.collectionView?.reloadItems(at: indexPaths)
+                })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
+                    self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                      at: .centeredVertically,
+                                                      animated: true)
+                }
+            }
+        }
+    }
+    
     override public func viewDidLoad()
     {
         super.viewDidLoad()
