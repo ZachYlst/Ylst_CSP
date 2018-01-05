0f4d2edc39d7cb8e5562720fc5c88b4b1085d7c8
ZachYlst <ZachYlst@users.noreply.github.com>
2018-01-05 09:28:17 -0700

    Rando-Commit.

--- Ylst_CSP/Controller/DataView/BucketItemCell.swift
+++ Ylst_CSP/Controller/DataView/BucketItemCell.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class BucketItemCell: UITableViewController
 {
     override public func viewDidLoad()
--- Ylst_CSP/Model/Player.swift
+++ Ylst_CSP/Model/Player.swift
@@ -90,21 +90,21 @@ public class Player: SKSpriteNode
         {
             return
         }
-//        else
-//        {
-//            canFire = false
-//            let bullet = PlayerLaser(imageName: "laser", bulletSound: "laserSound.mp3")
-//            bullet.position.x = self.position.x
-//            bullet.position.y = self.position.y + self.size.height/2
-//            scene.addChild(bullet)
-//            let moveBulletAction = SKAction.move(to: CGPoint(x: self.position.x, y: scene.size.height + bullet.size.height), duration: 1.0)
-//            let removeBulletAction = SKAction.removeFromParent()
-//            bullet.run(SKAction.sequence([moveBulletAction, removeBulletAction]))
-//            let waitToEnableFire = SKAction.wait(forDuration: 0.5)
-//            run(waitToEnableFire, completion:
-//                {
-//                    self.canFire = true
-//                })
-//        }
+        else
+        {
+            canFire = false
+            let bullet = PlayerLaser(imageName: "laser", bulletSound: "laserSound.mp3")
+            bullet.position.x = self.position.x
+            bullet.position.y = self.position.y + self.size.height/2
+            scene.addChild(bullet)
+            let moveBulletAction = SKAction.move(to: CGPoint(x: self.position.x, y: scene.size.height + bullet.size.height), duration: 1.0)
+            let removeBulletAction = SKAction.removeFromParent()
+            bullet.run(SKAction.sequence([moveBulletAction, removeBulletAction]))
+            let waitToEnableFire = SKAction.wait(forDuration: 0.5)
+            run(waitToEnableFire, completion:
+                {
+                    self.canFire = true
+                })
+        }
     }
 }
